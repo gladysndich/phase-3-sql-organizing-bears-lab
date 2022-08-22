9cc980f4e4c517893b403c08f3b26693b2d9d21e
gladysndich <itsgladysbrian@gmail.com>
2022-08-22 18:59:22 +0300

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 7, "M", "Gray", "sweet", 0),
+(2,"Rowdy", 5, "M", "Brown", "Calm", 1),
+(3,"Tabitha", 3, "F", "White-Patched", "Excited", 1),
+(4,"Sargeant Brown", 9, "M", "White", "Clumsy", 0),
+(5,"Melissa", 3, "F", "Light brown", "Aggressive", 1),
+(6,"Grinch", 2, "M", "Black", "Friendly", 1),
+(7,"Wendy", 13, "F", "Brown", "Curious", 1),
+(8,null, 5, "M", "Red", "Gentle", 0);
